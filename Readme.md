b64530c71cf0c11f4da321b36375df14d65b07a4
Gracielle da silva <graciellessv@gmail.com>
2025-03-25 23:57:12 -0300

    To-do list

---
+++ Readme.md
@@ -0,0 +1,15 @@
+#To-do-list | 
+
+Este é um projetinho simples de uma **To-Do List**, onde você pode adicionar e deletar tarefas de maneira prática.
+
+Para acessar o projeto completo, clique [aqui]( )
+
+## 🛠️ Tecnologias Utilizadas
+- **HTML5** 
+- **CSS3** 
+- **JavaScript**
+
+---
+<p align="center">
+Desenvolvido com 🧡
+</p>
--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+
+let input = document.querySelector("input"); 
+
 function adicionarTarefa() {
-  let input = document.querySelector("input").value;
+  let inputValor = input.value.trim(); 
+
+  if (!inputValor) return; 
 
   let li = document.createElement("li");
-  document.querySelector("ul").appendChild(li); // aqui falamos que o ul que ta no html vai receber o li como filho, e como n vamos pegar ele pra usar aq, vamos pegar apenas para falar onde o li vai ser adicionado, n precisa de let
+  document.querySelector("ul").appendChild(li);
 
-  li.innerHTML = input + "<span class='deletar' onclick='deletarTarefa(this)'>🗑️</span>";
+  li.innerHTML = `${inputValor} <span class='deletar' onclick='deletarTarefa(this)'>🗑️</span>`;
 
-  document.querySelector("input").value = "";
+  input.value = ""; 
 }
 
-function deletarTarefa(li){
-  li.parentElement.remove()
+input.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    adicionarTarefa();
+  }
+});
+
+function deletarTarefa(elemento) {
+  elemento.parentElement.remove(); 
 }
